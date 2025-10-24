60ef6787481c1e6cb9801e3ca52a563869785264
Goodness E. Eboh <ebohgoodness.t@gmail.com>
2025-10-24 01:16:08 +0100

    Updated backend file

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   # Remote state on UpCloud Object Storage (S3-compatible)
   backend "s3" {
-    bucket                      = "Your-bucket-name" # change to your bucket
-    key                         = "your-preferred-key-environment/terraform.tfstate"
-    endpoint                    = "Your-bucket-endpoint" # change to your endpoint
-    region                      = "de-fra1"              # required by S3 API, any string
+    bucket                      = "my-bucket-1" # change to your bucket
+    key                         = "default/terraform.tfstate"
+    endpoint                    = "https://gkf4e.upcloudobjects.com" # change to your endpoint
+    region                      = "de-fra1"                          # required by S3 API, any string
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
@@ -15,7 +15,7 @@ terraform {
   # Client-side encryption for state and plan (OpenTofu 1.7+)
   encryption {
     key_provider "pbkdf2" "main" {
-      passphrase = "our-Random-Phrase" #Preferrably via environment variables
+      passphrase = "jwekdewkedwjidwkjdnwkjdnwdjn" #Preferrably via environment variables
     }
 
     method "aes_gcm" "default" {
